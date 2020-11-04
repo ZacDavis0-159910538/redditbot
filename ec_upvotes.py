010b808eb126c8439d4f698880fbe6e1f5dcc596
ZacDavis0-159910538 <70255063+ZacDavis0-159910538@users.noreply.github.com>
2020-11-03 23:19:31 -0800

    Update ec_upvotes.py

--- ec_upvotes.py
+++ ec_upvotes.py
@@ -6,15 +6,14 @@ import time
 import os
 print(os.getcwd())
 
-reddit = praw.Reddit('bot2')
+reddit = praw.Reddit('bot1')
 reddit_debate_url = 'https://www.reddit.com/r/csci040temp/comments/jl1zec/people_embrace_rebranding_of_proud_boys_to_poor/'
 submission = reddit.submission(url=reddit_debate_url)
 subreddit = reddit.subreddit('csci040temp')
 
 #EC upvotes submissions and comments
 comment = reddit.comment in submission.comments.list()
-upvoted_comments = []
-upvoted_submissions = []
+
 '''
 make new python file
 add try and excepts, add time sleep
@@ -23,8 +22,14 @@ print('Task 2 exception found')
     time.sleep(5)
     print('done sleeping')
 '''
-for i in range(1):
-    name = ('Joe', 'Biden', 'Vice President', 'VP')
+a = 0
+for i in range(10):
+    a += 1
+    upvoted_comments = []
+    upvoted_submissions = []
+    print('bot'+str(a)+'starting up')
+    reddit = praw.Reddit('bot'+str(a))
+    time.sleep(5)
     for s in subreddit.new(limit=10):
         #if type(s) is praw.models.reddit.submission:
             if 'Joe' or 'Biden' or 'VP' or 'Vice President' in str(s.title):
@@ -35,12 +40,12 @@ for i in range(1):
                         print('submission upvote success')
                         print(upvoted_submissions)
                     except:
-                        print('submission upvote exception found')
+                        print('submission upvote exception found/already upvoted by this bot')
                         print('starting to sleep')
                         time.sleep(5)
                         print('done sleeping')
-    
-    for c in submission.comments.list():
+    commlist = submission.comments.list()
+    for c in commlist[0:10]:
         if type(c) is praw.models.reddit.comment.Comment:
             if 'Joe' or 'Biden' or 'VP' or 'Vice President' in str(c.body):
                 if c not in upvoted_comments:
